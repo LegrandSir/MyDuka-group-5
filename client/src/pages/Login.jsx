8b77174aef9a855079040b2e64f9c0ded2406bed
Ellis Lunayo <ellislunayo@gmail.com>
2025-09-16 21:22:25 +0300

    installed the react router dom dependency

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+    return (
+        <div>
+            <p>Welcome To</p>
+            <h2>My Duka</h2>
+            <p>Sign in to manage your inventory and track your business</p>
+        </div>
+    )
+
+}
+
