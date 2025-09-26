92d7ee8305d7617742d79abc547ee7a11d5e21d2
Faith Wangari <njihiafaith@gmail.com>
2025-09-26 18:45:46 +0300

    Adds to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
