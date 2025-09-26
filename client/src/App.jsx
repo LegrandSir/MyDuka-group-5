25cea3572692f208069a406b5cd7d5715fe79010
levy <levngi.muchiri@gmail.com>
2025-09-26 20:06:39 +0300

    feat: Connect client jsx code to server endpoints

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes"; 
-// import AdminDashboard  from "./pages/ClerkDashboard"
+
 
 
 function App() {
   return (
   <BrowserRouter>
-    <AppRoutes />
+    <App />
   </BrowserRouter>
   );
 }
